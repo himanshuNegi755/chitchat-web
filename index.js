1ede6f24e3bd0f48040c6c4413851b520047a1f9
Himanshu <himanshu.hn.1998@gmail.com>
2020-09-27 17:58:12 +0530

    deleted the default src folder in client and made new one, along with chat and join component

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
-var express = require('express');
-var cors = require('cors')
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-require('./model/user');
-
-var keys = require('./config/keys');
-var cookieSession = require('cookie-session');
-
-const corsOptions = {
-  credentials: true,
-  origin: true
-}
-
-require('./config/passport-setup');
-
-var passport = require('passport');
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//import routes
-const authRoutes = require("./routes/auth-routes");
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cors(corsOptions));
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(cookieSession({
-    name: 'login',
-    maxAge: 60*60*1000*24,  // 7*24* add later after completion of site
-    keys: [keys.session.cookieKey]
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-//set up routes
-app.use('/api', authRoutes);
-
-// listining for port
-app.listen(PORT, function() {
-    console.log(`Mento API running on port ${PORT}....`);
+io.on('connection', (socket) => {
+  console.log('We have a new connection!!!');
+  
+  socket.on('disconnect', () => {
+    console.log('User had left!!!');
+  })
 });
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
