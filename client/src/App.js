76a4b9571e57b40756ccd7956e1d6f1885674878
Himanshu <himanshu.hn.1998@gmail.com>
2020-09-28 13:32:56 +0530

    chat application is completed need to make necessary changes for the chitchat application, including login etc..

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Chat from './components/Chat';
-import Join from './components/Join';
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
