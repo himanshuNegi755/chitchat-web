5a96ca915cc1e29405bd046b63e60d1bbd696084
Himanshu <himanshu.hn.1998@gmail.com>
2020-10-31 20:12:48 +0530

    minor changes to merge commit

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,11 +7,9 @@ const Footer = () => {
               <div className="container-fluid contact-details">
                   <h4 className="contact-us">CONTACT US</h4>
                   <p className="contact-text">For any queries please mail us and we will reply as soon as possible with a solution.</p>
-                  <br></br>
-                  <a className="contact-icons" href="mailto:thekatohome@gmail.com"><i className="fas fa-envelope fa-2x" aria-hidden="true"></i> </a>
-                  <a className="contact-icons" href="https://wa.me/message/MRZMBG4DLYZ2I1"><i className="fab fa-whatsapp fa-2x"></i></a>
+                  <a className="contact-icons" href="mailto:thekatohome@gmail.com"><i className="fas fa-envelope fa-2x" aria-hidden="true"></i></a>
                   <hr className="contact-divider"></hr>
-                  <p className="copyright">© COPYRIGHT 2020 DINEEZY.</p>
+                  <p className="copyright">© COPYRIGHT 2020 CHIT-CHAT.</p>
               </div>
           </footer>
         );
